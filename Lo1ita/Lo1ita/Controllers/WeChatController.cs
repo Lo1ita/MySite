1bb462cbda5fa9cb8aaedd12e1d791db9469970a
Lo1ita <32256789+Lo1ita@users.noreply.github.com>
2017-11-07 00:37:52 +0800

    framwork4,5

---
+++ Lo1ita/Lo1ita/Controllers/WeChatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lo1ita.Controllers
+{
+    public class WeChatController : Controller
+    {
+        // GET: WeChat
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
