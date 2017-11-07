f1a0cd0ff23f4c609b0978de066adfd00cc9e0e3
Lo1ita <32256789+Lo1ita@users.noreply.github.com>
2017-11-07 23:24:20 +0800

    什么都没做

---
+++ Lo1ita/Lo1ita/Common/HttpHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lo1ita.Common
+{
+    public class HttpHelper
+    {
+        public 
+    }
+}
--- Lo1ita/Lo1ita/Controllers/WeChatController.cs
+++ Lo1ita/Lo1ita/Controllers/WeChatController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +9,30 @@ namespace Lo1ita.Controllers
 {
     public class WeChatController : Controller
     {
+        static string CORPID = ConfigurationManager.AppSettings["CorpID"];
+        static string SECRET= ConfigurationManager.AppSettings["Secret"];
+        static DateTime _lastGetTimeOfAccessToken = DateTime.Now.AddSeconds(-7201);
         // GET: WeChat
         public ActionResult Index()
         {
             return View();
+        }
+
+        public static GetAccessToken()
+        {
+            try
+            {
+                if(_lastGetTimeOfAccessToken < DateTime.Now)
+                {
+                    string url = string.Format("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={0}&corpsecret={1}", CORPID, SECRET);
+                    string responseText= HttpHelper.Instance.get(url);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
