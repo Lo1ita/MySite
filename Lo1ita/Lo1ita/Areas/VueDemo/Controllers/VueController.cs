040927cf8f08d8e0f62d8d8e24f93349241ac6a1
Lo1ita <32256789+Lo1ita@users.noreply.github.com>
2018-01-17 17:29:05 +0800

    学习Vue
    照着文档写Demo

---
+++ Lo1ita/Lo1ita/Areas/VueDemo/Controllers/VueController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lo1ita.Areas.VueDemo.Controllers
+{
+    public class VueController : Controller
+    {
+        // GET: VueDemo/Vue
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lo1ita/Lo1ita/Areas/VueDemo/VueDemoAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Lo1ita.Areas.VueDemo
+{
+    public class VueDemoAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "VueDemo";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "VueDemo_default",
+                "VueDemo/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
