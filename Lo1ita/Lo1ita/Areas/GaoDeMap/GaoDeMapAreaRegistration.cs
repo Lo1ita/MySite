6cf65eb2cb6f3cf22704af07f1aefa778acbda5f
Lo1ita <32256789+Lo1ita@users.noreply.github.com>
2018-03-07 17:55:44 +0800

    高德地图测试

---
+++ Lo1ita/Lo1ita/Areas/GaoDeMap/GaoDeMapAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Lo1ita.Areas.GaoDeMap
+{
+    public class GaoDeMapAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GaoDeMap";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GaoDeMap_default",
+                "GaoDeMap/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
